948650ebe568d796b29d5a9a3d2a436380a675b1
Angelica Miranda <mirandas.angelica@gmail.com>
2020-05-20 23:51:15 -0500

    add helmet xss & boolean parse

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+var boolParser  = require('express-query-boolean');
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
+const helmet          = require('helmet');
 
 var app = express();
 
@@ -19,6 +21,9 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(boolParser());
+
+app.use(helmet.xssFilter())
 
 //Sample front-end
 app.route('/:project/')
